ddd245092de706eb82b830260efa6e5122f17d77
Hiroki <ore01100001binary@gmail.com>
2025-07-01 00:53:19 +0900

    💪shakyo-18

---
+++ src/shakyo-18/components/UserItem.tsx
@@ -0,0 +1,24 @@
+import React, { memo } from 'react';
+import { User } from '../types/User';
+
+type Props = {
+    user: User;
+    onToggle: (id: number) => void;
+};
+
+const UserItem: React.FC<Props> = memo(({user, onToggle}) => {
+    const { id, name, active } = user;
+
+    return (
+        <li style={{
+                color: active ? 'green' : 'gray',
+                cursor: 'pointer'
+            }}
+            onClick = {() => onToggle(id)}
+        >
+            {name} {active ? "(active)" : "(inactive)"}
+        </li>
+    );
+});
+
+export default UserItem;
